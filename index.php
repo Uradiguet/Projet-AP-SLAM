0844381715f30e9a06238cd58b2666109c9291e1
Uradiguet <ugo.radiguet@sts-sio-caen.info>
2023-12-22 22:48:09 +0100

    structuration du php

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
 		<link rel="stylesheet" href="./asset/style.css">
-		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@900&display=swap" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@900&display=swap" rel="stylesheet">
+        <link rel="shortcut icon" href="">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
         <script src="https://www.google.com/recaptcha/api.js" async defer></script>
         <?php require_once './yaml/yaml.php'; ?>
     </head>
