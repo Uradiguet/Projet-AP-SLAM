1394d30a29849668167ba3952bdbf4fd23e74ff9
Uradiguet <Ugo.radiguet@sts-sio-caen.info>
2023-12-23 00:45:05 +0100

    VM

--- index.php
+++ index.php
@@ -10,7 +10,10 @@
         <link rel="shortcut icon" href="">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
         <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-        <?php require_once './yaml/yaml.php'; ?>
+        <?php 
+include_once './vendor/autoload.php';
+require_once './yaml/yaml.php'; 
+?>
     </head>
     <body>
 
